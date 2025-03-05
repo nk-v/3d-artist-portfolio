0eb7d851409242044d516a6b55cfa1fd112a2fef
vidmanov <vidmanov@mercurydevelopment.com>
2025-03-05 23:42:48 +0700

    feat: Add data

---
+++ src/data/projects.ts
@@ -0,0 +1,51 @@
+type TProject = {
+  webUrl: string;
+  picUrl: string;
+  title: string;
+  description: string;
+};
+
+export const PROJECTS: TProject[] = [
+  {
+    webUrl: "https://www.instagram.com/p/CyX6H4Jt4Q4/",
+    picUrl: `/shell-thumb.jpg`,
+    title: "SHELL".toLowerCase(),
+    description: "procedural fabric",
+  },
+  {
+    webUrl: "https://www.instagram.com/p/CyrJUVvNRUI/",
+    picUrl: "/inner-child-thumb.jpg",
+    title: "INNER CHILD".toLowerCase(),
+    description: "static",
+  },
+  {
+    webUrl: "https://www.instagram.com/p/CzRC3hPNU53/",
+    picUrl: "/fresh-breezer-thumb.jpg",
+    title: "FRESH BREEZER".toLowerCase(),
+    description: `winner #1 of MediaArt animation contest:\r\nfake advertisment of non-functional product`,
+  },
+  {
+    webUrl: "https://www.instagram.com/p/Cg187UesQ-2/",
+    picUrl: "/kodak-thumb.jpg",
+    title: "KODAK".toLowerCase(),
+    description: "procedural fabric",
+  },
+  {
+    picUrl: "/meta-verse-thumb.jpg",
+    webUrl: "https://www.instagram.com/p/CiXo3jHA1uH/",
+    title: "Meta Verse",
+    description: "Animation for contest",
+  },
+  {
+    picUrl: "/chelobnk-thumb.jpg",
+    webUrl: "https://www.instagram.com/p/C12MHxTNo-W/",
+    title: "chelobnk",
+    description: "Animation for contest",
+  },
+  {
+    picUrl: "/domino-thumb.jpg",
+    webUrl: "https://www.instagram.com/p/C6pC86NN3fP/",
+    title: "domino",
+    description: "self project",
+  },
+];
---
+++ src/data/skills.ts
@@ -0,0 +1,13 @@
+export const SKILLS = [
+  "Cinema 4D",
+  "Redshift",
+  "Houdini",
+  "Blender",
+  "After Effects",
+  "Octane Render",
+  "ZBrush",
+  "Unreal Engine",
+  "Adobe Creative Suite",
+  "Motion Design",
+  "Animation",
+];
